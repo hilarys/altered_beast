9c94ca5b18df96649b3b334b6deae6ddedd89b6b
Gordon DuQuesnay <gduquesnay@gmail.com>
2010-02-21 07:03:22 +0900

    fix some route errors, hide the 'set' button if user has javascript.

--- app/controllers/monitorships_controller.rb
+++ app/controllers/monitorships_controller.rb
@@ -2,18 +2,20 @@ class MonitorshipsController < ApplicationController
   before_filter :login_required
 
   def create
-    @monitorship = Monitorship.find_or_initialize_by_user_id_and_topic_id(current_user.id, params[:topic_id])
+    topic_id = Topic.find_by_permalink(params[:topic_id]).id
+    @monitorship = Monitorship.find_or_initialize_by_user_id_and_topic_id(current_user.id, topic_id)
     @monitorship.update_attribute :active, true
     respond_to do |format| 
-      format.html { redirect_to topic_path(params[:forum_id], params[:topic_id]) }
+      format.html { redirect_to forum_topic_path(params[:forum_id], params[:topic_id]) }
       format.js
     end
   end
   
   def destroy
-    Monitorship.update_all ['active = ?', false], ['user_id = ? and topic_id = ?', current_user.id, params[:topic_id]]
+    topic_id = Topic.find_by_permalink(params[:topic_id]).id
+    Monitorship.update_all ['active = ?', false], ['user_id = ? and topic_id = ?', current_user.id, topic_id]
     respond_to do |format| 
-      format.html { redirect_to topic_path(params[:forum_id], params[:topic_id]) }
+      format.html { redirect_to forum_topic_path(params[:forum_id], params[:topic_id]) }
       format.js
     end
   end
